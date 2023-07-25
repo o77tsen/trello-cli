7708a6b5bf8cf55eda74ce52bc6d578b76c8df61
o77tsen <astrowavefunction@gmail.com>
2023-07-25 10:21:39 +1000

    initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/adlio/trello"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+
+	appKey := os.Getenv("TRELLO_KEY")
+	token := os.Getenv("TRELLO_TOKEN")
+	boardId := os.Getenv("TRELLO_BOARD_ID")
+
+	client := trello.NewClient(appKey, token)
+
+	board, err := client.GetBoard(boardId, trello.Defaults())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(board)
+}
