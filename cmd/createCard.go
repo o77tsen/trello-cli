631d5d427fb75a353465d3a902e05f69ba658079
o77tsen <astrowavefunction@gmail.com>
2023-07-27 11:44:42 +1000

    Edit createCard.go

--- cmd/createCard.go
+++ cmd/createCard.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 
 	"github.com/adlio/trello"
-	"github.com/joho/godotenv"
 	"github.com/manifoldco/promptui"
+	trelloClient "github.com/o77tsen/trello-cli/client"
 	"github.com/spf13/cobra"
 )
 
@@ -35,17 +35,9 @@ func init() {
 }
 
 func createCard() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading env file:", err)
-		os.Exit(1)
-	}
+	client := trelloClient.NewTrelloClient()
 
-	appKey := os.Getenv("TRELLO_KEY")
-	token := os.Getenv("TRELLO_TOKEN")
 	boardId := os.Getenv("TRELLO_BOARD_ID")
-
-	client := trello.NewClient(appKey, token)
 
 	board, err := client.GetBoard(boardId, trello.Defaults())
 	if err != nil {
